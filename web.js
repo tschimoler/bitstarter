0508dc1dbba1808a057685b74f50949fa40fb952
Ubuntu <ubuntu@ip-10-236-154-39.ec2.internal>
2013-07-18 22:57:10 +0000

    made index.html pretty and responsive

--- web.js
+++ web.js
@@ -1,13 +1,16 @@
 var fs = require('fs');
 var express = require('express');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/bootstrap')); 
+app.use(express.static(__dirname + '/font-awesome'));
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html').toString());
+  response.send(fs.readFileSync(htmlfile).toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
